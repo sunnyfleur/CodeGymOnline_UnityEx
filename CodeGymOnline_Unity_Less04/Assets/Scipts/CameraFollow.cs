096c1337a2fb0501d325c56c64a6d64da405bd1e
Tran Hoang Kiet <116096297+sunnyfleur@users.noreply.github.com>
2023-08-12 14:51:21 +0700

    less04

---
+++ CodeGymOnline_Unity_Less04/Assets/Scipts/CameraFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] protected Transform targetObject;
+    [SerializeField] protected float followSpeed = 3f;
+    [SerializeField] protected Vector3 offset = new Vector3(3f, 5f, 7f);
+    void Start()
+    {
+        LoadPlayerObject();
+    }
+    private void Reset()
+    {
+        LoadPlayerObject();
+    }
+    public void LoadPlayerObject()
+    {
+        if (targetObject != null) return;
+        else
+            this.targetObject = GameObject.Find("Player").transform;    
+    }
+
+    void Update()
+    {
+        FollowOject();
+    }
+
+    public void FollowOject()
+    {
+        Vector3 targetOffsetPosition= targetObject.position+offset;
+        Vector3 cameraNewPosition = Vector3.Lerp(transform.parent.position, targetOffsetPosition, followSpeed*Time.deltaTime);
+        transform.parent.position = cameraNewPosition;
+        transform.parent.LookAt(targetObject.position);
+    }
+}
