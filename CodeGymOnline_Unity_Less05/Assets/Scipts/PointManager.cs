4b0d2f063df5ea49fa974b25f281d4dd27c69119
Tran Hoang Kiet <116096297+sunnyfleur@users.noreply.github.com>
2023-08-26 15:06:04 +0700

    Less09

---
+++ CodeGymOnline_Unity_Less05/Assets/Scipts/PointManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    [SerializeField] protected float currentPoint;
+
+    [SerializeField] protected float maxPoint;
+}
--- CodeGymOnline_Unity_Less05/Assets/Scipts/RotateObject.cs
+++ CodeGymOnline_Unity_Less05/Assets/Scipts/RotateObject.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RotateObject : MonoBehaviour
 {
-    [SerializeField] private Transform targetObject;
+    [SerializeField] private GameObject targetObjectModel;
+    [SerializeField] private float coordinateY;
+    [SerializeField] private float coordinateX;
+    [SerializeField] private float rotationSpeed=20f;
 
+    private void Reset()
+    {
+        LoadRotationObject();
+    }
+    private void Start()
+    {
+        LoadRotationObject();
+    }
+    private void Update()
+    {
+        Rotate();
+    }
+    void LoadRotationObject()
+    {
+       
+
+        this.targetObjectModel =GameObject.Find("Carrot1");
+    }
     void Rotate()
     {
-
-        
+        Vector3 eulerAngle= new Vector3(0,1,0);
+        this.targetObjectModel.transform.Rotate(eulerAngle * rotationSpeed * Time.deltaTime);
     }
 }
